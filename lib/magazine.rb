8702427868f39c534ccadd8eee57f3dc58b9b8e1
Milhan Osman <milhanomarosman@gmail.com>
2023-05-28 14:42:03 +0300

    added some required methods

--- lib/magazine.rb
+++ lib/magazine.rb
@@ -14,7 +14,17 @@ class Magazine
     @@all
   end
 
-  
+  def self.find_by_name(name)
+    self.all.find { |magazine| magazine.name == name }
+  end
+
+  def article_titles
+    Article.all.select { |article| article.magazine == self }.map { |article| article.title }
+  end
+
+  def contributing_authors
+    Article.all.select { |article| article.magazine == self }.map { |article| article.author }.group_by { |author| author }.select { |author, articles| articles.size > 2 }.keys
+  end
 
   def to_s
     @name
